28b9bed9fb229e752b6cb2dfa45f66ada1649193
karrless <konstantin.geydarov@gmail.com>
2024-04-19 22:36:43 +0300

    run.py +logger +db manage

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
-import logging
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-import src.database as db
-
-logging.basicConfig(level=logging.DEBUG, filename='log.log', filemode='w',
-                    format='%(asctime)s: %(levelname)s — %(message)s')
-
-DEBUG = int(os.getenv('DEBUG'))
-
 
 if __name__ == '__main__':
+    import logging
+    import os
+    import src.database as db
+
+    logging.basicConfig(level=logging.DEBUG, filename='log.log', filemode='w',
+                        format='%(asctime)s: %(levelname)s — %(message)s')
+
+    DEBUG = int(os.getenv('DEBUG'))
+
     if DEBUG:
         # db.drop_all()
         db.create()
-    db.connect()
+        db.connect()
+        # from src.dormitory.comforts import add_comforts
+        # add_comforts()
+    else:
+        db.connect()
