1b4d5cb3752b11ddb7e4b1ec6e0c204863191cc2
karrless <konstantin.geydarov@gmail.com>
2024-04-20 13:33:01 +0300

    update bot/methods.py

--- src/bot/methods.py
+++ src/bot/methods.py
@@ -1,14 +1,13 @@
-from typing import overload
-
 from sqlalchemy.orm import Session
 
-from ..database import with_connection, write
+from ..database import with_connection
 from ..database.models import User, Room
 
 
 @with_connection
 def get_user(session: Session, peer_id: int) -> User | None:
     return session.query(User).where(User.peer_id == peer_id).first()
+
 
 @with_connection
 def get_users(session: Session, room: Room) -> list[User] | None:
