d2a33fd4de5c0ee5c2fbb7e7eecbf6a17edf221d
karrless <konstantin.geydarov@gmail.com>
2024-07-27 03:43:13 +0300

    refactor
    # Надо сделать принятие вопроса модераторами, но пока все работает норм вроде еще бы научиться нормально писать, а не так, как я пишу...

--- src/bot/handlers/QA.py
+++ src/bot/handlers/QA.py
@@ -97,7 +97,7 @@ async def write_question(message: Message):
     requests.post('https://api.vk.com/method/messages.sendReaction',
                   data={'peer_id': message.peer_id,
                         'cmid': message.conversation_message_id,
-                        'reaction_id': 39,
+                        'reaction_id': 10,
                         'access_token': os.getenv('VK_API1'),
                         'v': '5.199'})
 
